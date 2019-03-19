31a132ec1143b7a6a80412021ea18c5f02e0addd
iangifford <lightyagamie10@gmail.com>
2019-03-19 10:18:45 -0700

    Building out Populate method

--- NumbersGame/NumbersGame/Program.cs
+++ NumbersGame/NumbersGame/Program.cs
@@ -8,22 +8,37 @@ namespace NumbersGame
         {
             Console.WriteLine("Numbers Game Yay! but lets break it");
 
-            //start sequence method called
+            //StartSequence method called
             StartSequence();
             Console.WriteLine();
 
-        } //end of main
+        } //end of Main
 
+        //StartSequence method with logic
         static void StartSequence()
         {
             //Prompt to do the thingy
-            Console.WriteLine("Enter a Numero greater than Zero if you are a hero");
+            Console.WriteLine("Enter a Number greater than Zero if you are a hero");
             //User input
             string userResponse = Console.ReadLine();
             //Conversion from a string to an integer
-            int numero = Convert.ToInt32(userResponse);
+            int number = Convert.ToInt32(userResponse);
             //Instantiate a new integer array
-            int[] newArray = new int[0];
+            int[] newArray = new int[number];
+            //Calls Populate method
+            Populate(newArray);
+
+        }//end of StartSequence
+
+        static int Populate(int[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Please enter a number 1 of 6");
+                string userResponse = Console.ReadLine();
+                int numero = Convert.ToInt32(userResponse);
+
+            }
         }
             
     }//end of class
